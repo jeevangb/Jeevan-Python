352abbe0d0b03c16c5e487abea1c3fc7adc97dd0
jeevan <jeevanindian01@gmail.com>
2024-09-04 20:59:30 +0530

    [JEEVAN] ADD: Data operators

---
+++ Operators/operators.py
@@ -0,0 +1,23 @@
+#Program for Minimum of 3 Numbers
+a=10
+b=20
+c=15
+min=a if a<b and a<c else b if b<c else c
+print(min)
+
+#identity operatot
+# is - content comparision
+# == - object address comparision
+a=1
+b=2
+print(a is b)
+
+#math module
+import math
+print(math.sqrt(16))
+print(math.pi)
+
+#areaof circle 
+from math import pi
+r=3
+print("Area: ",pi*r**2)
